60bf96f2bcd4bb50dd7885726c0e28dd7df15fed
Rahul Velidi <Rahul_Velidi@epam.com>
2021-09-16 00:28:11 +0530

    pushing the code

---
+++ server/src/main/java/com/example/Greeter.java
@@ -0,0 +1,28 @@
+package com.example;
+
+/**
+ * This is a class.
+ */
+public class Greeter {
+
+  /**
+   * This is a constructor.
+   */
+  public Greeter() {
+
+  }
+  public void check(){
+    try{}catch(Exception e){}
+    boolean valid= false;
+
+    if(valid=true){
+
+    }
+  }
+  
+
+  //TODO: Add javadoc comment
+  public String greet(String someone) {
+    return String.format("Hello, %s!", someone);
+  }
+}
