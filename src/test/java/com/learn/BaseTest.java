089edc9f34854a81006d1d6dbd93f9cbeeeb5a60
Rahul Velidi <Rahul_Velidi@epam.com>
2021-09-29 20:20:01 +0530

    testcases code

---
+++ src/test/java/com/learn/BaseTest.java
@@ -0,0 +1,37 @@
+package com.learn;
+
+import java.net.MalformedURLException;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseTest {
+	WebDriver driver = null;
+
+	@BeforeClass
+	@Parameters(value = { "browser" })
+	public void setupTest(String browser) throws MalformedURLException {
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--incognito");
+		// options.addArguments("headless");
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver(options);
+
+	}
+
+	public WebDriver getDriver() {
+		return driver;
+	}
+
+	@AfterClass
+	public void tearDown() {
+		getDriver().quit();
+	}
+
+}
---
+++ src/test/java/com/learn/FirstTest.java
@@ -0,0 +1,84 @@
+package com.learn;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class FirstTest extends BaseTest {
+
+	@Test(priority = 1)
+	public void titleCheck() {
+		Reporter.log(String.format("Title verification testcase %s", Thread.currentThread().getId()), true);
+		driver = getDriver();
+		driver.get("http://65.0.188.190:8080/webapp-5.0.0/");
+
+		Assert.assertEquals(driver.findElement(By.xpath("/html/body/h2")).getText(), "Modal Signup Form");
+		Reporter.log(String.format("application name verified %s", Thread.currentThread().getId()), true);
+	}
+
+	@Test(priority = 2)
+	public void emailLabelTest() {
+		Reporter.log(String.format("Environment test %s", Thread.currentThread().getId()), true);
+
+		driver.findElement(By.xpath("/html/body/button")).click();
+		Assert.assertEquals(driver.findElement(By.xpath("//*[@id='id01']/form/div/label[1]/b")).getText(), "Email");
+		Reporter.log(String.format("Test for env verified %s", Thread.currentThread().getId()), true);
+	}
+
+	@Test(priority = 3)
+	public void emptySignupCheck() {
+		Reporter.log(String.format("Environment test %s", Thread.currentThread().getId()), true);
+
+		WebElement submit = driver.findElement(By.xpath("//*[@id='id01']/form/div/div/button[2]"));
+		submit.click();
+
+		Assert.assertTrue(driver.findElement(By.xpath("//*[@id='id01']/form/div/input[1]"))
+				.equals(driver.switchTo().activeElement()));
+		Reporter.log(String.format("Test for env verified %s", Thread.currentThread().getId()), true);
+	}
+
+	@Test(priority = 4)
+	public void passwordLabelCheck() {
+		Reporter.log(String.format("Environment test %s", Thread.currentThread().getId()), true);
+
+		Assert.assertEquals(driver.findElement(By.xpath("//*[@id='id01']/form/div/label[2]/b")).getText(), "Password");
+		Reporter.log(String.format("Test for env verified %s", Thread.currentThread().getId()), true);
+	}
+
+	@Test(priority = 5)
+	public void passwordRepwdcheck() {
+		Reporter.log(String.format("Environment test %s", Thread.currentThread().getId()), true);
+		WebElement password = driver.findElement(By.xpath("//*[@id='id01']/form/div/input[2]"));
+		WebElement repassword = driver.findElement(By.xpath("//*[@id='id01']/form/div/input[3]"));
+		password.sendKeys("test");
+		repassword.sendKeys("test");
+		Assert.assertEquals(password.getText(), repassword.getText());
+		Reporter.log(String.format("Test for env verified %s", Thread.currentThread().getId()), true);
+	}
+
+	@Test(priority = 6)
+	public void missingmandatoryfieldsCheck() {
+		Reporter.log(String.format("Environment test %s", Thread.currentThread().getId()), true);
+
+		WebElement submit = driver.findElement(By.xpath("//*[@id='id01']/form/div/div/button[2]"));
+		submit.click();
+
+		Assert.assertTrue(driver.findElement(By.xpath("//*[@id='id01']/form/div/input[1]"))
+				.equals(driver.switchTo().activeElement()));
+		Reporter.log(String.format("Test for env verified %s", Thread.currentThread().getId()), true);
+	}
+
+	@Test(priority = 7)
+	public void cancelBtnCheck() {
+		Reporter.log(String.format("Environment test %s", Thread.currentThread().getId()), true);
+
+		WebElement cancel = driver.findElement(By.xpath("//*[@id='id01']/form/div/div/button[1]"));
+		cancel.click();
+
+		Assert.assertEquals(driver.findElement(By.xpath("/html/body/h2")).getText(), "Modal Signup Form");
+		Reporter.log(String.format("Test for env verified %s", Thread.currentThread().getId()), true);
+	}
+
+}
